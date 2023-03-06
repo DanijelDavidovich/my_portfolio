417a798e36180be8b653d05ee63770460f7a8e90
Danijel Davidovic <danijeldavidovic1995@gmail.com>
2023-03-06 15:46:13 +0100

    End

--- my_portfolio/src/components/myProjects/myProjects.js
+++ my_portfolio/src/components/myProjects/myProjects.js
@@ -2,6 +2,7 @@ import classes from "./myProjects.module.css";
 import { useState } from "react";
 import omnifoodImage from "../images/omnifood.png";
 import dentilImage from "../images/dentil.png";
+import forkifyImage from "../images/forkify.png";
 import MyProject from "./myProject";
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -29,12 +30,23 @@ const MyProjects = () => {
         <MyProject
           img={dentilImage}
           header="Dentil"
-          shortAbout="Final project from the subject 'Software Design' from the third year of the Faculty of Electrical Engineering, Banja Luka. The project was created using HTML, CSS and JavaScript."
+          shortAbout="My final project from the subject 'Software Design' from the third year of the Faculty of Electrical Engineering, Banja Luka. The project was created using HTML, CSS and JavaScript."
           text="After creating the complete documentation, in the course 'Software Design' we started, as a team, to create the software itself. I was assigned to create the front-end part of the project, and this is the result of my work. The project was created using the skills acquired during the HTML/CSS course and the JavaScript course. Calendar functionality is done in JavaScript."
           linkLive="https://denrilweb.netlify.app"
           time={1000}
           delay={1000}
           fadeDirection="fade-left"
+        />
+
+        <MyProject
+          img={forkifyImage}
+          header="Forkify"
+          shortAbout="Final project on the course: 'The Complete JavaScript Course: From Zero to Expert!'"
+          text="Most of the HTML and CSS part of the project was done by the author, so the focus was almost exclusively on JavaScript. Through the development of the project, everything that was previously done individually throughout the entire course was applied. Creating the project included many things starting from the basics of JS, through how JS works in the background, what is JS engine and runtime, how DOM and Events work, CSS manipulation through JS, OOP JS, promises, async/await, AJAKS, fetch API, handling errors, modules, tooling, etc. The application allows you to search for recipes from the author's database, view the details of the selected recipe, save recipes (saved on the local store), enter your own recipes and send them to the database, etc."
+          linkLive="https://forkify-v1-app.netlify.app"
+          time={1000}
+          delay={1000}
+          fadeDirection="fade-right"
         />
       </div>
     </div>
@@ -50,8 +62,10 @@ export default MyProjects;
           img={}
           header=""
           shortAbout=""
-          linkLive ={}
-          linkCode={}
-          linkPopUp = {}
+          linkLive =""
+          linkCode=""
+          time={1000}
+          delay={1000}
+          fadeDirection=""
         /> */
 }
